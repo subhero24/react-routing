f4e8e15f9f06d13a3d4ead6053d5774fac7ba239
SubHero <github@bvdsoftware.be>
2020-04-24 01:22:31 +0200

    Updated contexts

--- source/contexts/location.js
+++ source/contexts/location.js
@@ -1,3 +1,5 @@
 import { createContext } from 'react';
 
+// We do not use the browsers location as the default for this context,
+// as the context is also used in other environments
 export default createContext();
