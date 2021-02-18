97eb7edd2532731c63da32c7819c34d883e1e194
SubHero <github@bvdsoftware.be>
2021-02-18 03:17:31 +0100

    Removed comment

--- source/hooks/use-latest-ref.js
+++ source/hooks/use-latest-ref.js
@@ -1,14 +1,9 @@
-import { useRef, useEffect } from 'react';
+import { useRef } from 'react';
 
 export default function useLatestRef(value) {
-	// We do not pass the value, as access to this should only
-	// happen in callbacks, after useEffect has already run
 	let ref = useRef();
 
 	ref.current = value;
-	// useEffect(() => {
-	// 	ref.current = value;
-	// });
 
 	return ref;
 }
